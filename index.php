61a21b18708bd95fb3f1856e57ad9d8827520c2a
amcmorrow <andrew.michael.mcmorrow@gmail.com>
2024-02-21 12:45:12 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-container=no">
+
+  <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.24/dist/css/uikit.min.css" />
+
+  <!-- Portfolio Site CSS -->
+
+<style>
+.container {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+.form-control {
+  width: 100%;
+}
+
+</style>
+
+
+  <title>Ecommerce Manager Portfolio</title>
+
+</head>
+
+<body>
+
+  <!-- Header -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Andrew McMorrow</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">Backstory</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Professional</a>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="/resume/">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#clients">Personal & Clients</a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://amcmorrow.com/gem/" target="_blank">Chatbot</a>
+        </li>
+    </div>
+  </div>
+</nav>
+
+
+  <!-- About Section -->
+  <section id="about" class="uk-section uk-section-small">
+    <div class="uk-container"><br><br>
+      <img class="uk-align-center" src="images/amm-signature.png" width="" height="" alt=""><br><br>
+      <h2 class="uk-text-center">Some background...</h2>
+      <p>Hello there. I'm Andrew McMorrow, an E-commerce Manager based in Minneapolis, MN. With a passion for technology and a love for the great outdoors, I've forged a unique path that blends creativity, problem-solving, and business acumen. My journey started in a small town where two rivers met, and there, I developed an insatiable curiosity and a deep appreciation for the natural world.
+<br><br>
+My career has been defined by a relentless pursuit of excellence in e-commerce management and digital marketing. Through my experiences at Revo Brands, I have optimized websites and wholesale dealer portals, utilizing data analysis and automation to drive sales and enhance user experiences. At Dakota Stones, I spearheaded successful ERP system migrations and website transitions, honing my technical skills and delivering measurable results. I have also contributed to the growth of businesses like Treasure Island, where I led digital media campaigns and managed website development and email marketing efforts.
+<br><br>
+With a degree in Visual Communications and a passion for continual learning, I stay ahead of the latest trends in e-commerce, user experience optimization, and data-driven decision making. My commitment to excellence and my ability to think outside the box have earned me a reputation as a savvy and valuable asset in the field.
+<br><br>
+Beyond my professional accomplishments, I remain grounded in the values instilled in me by my small-town upbringing. I'm grateful for the opportunities that have shaped my journey and inspired me to create innovative solutions that drive success for countless companies. My dedication to making a positive impact in the world, coupled with my adventurous spirit, fuels my unwavering passion for the thrill of the ride.</div>
+  </section>
+
+  <!-- Professional Projects Section -->
+ <style>
+  .card-img, .card-img-bottom, .card-img-top {
+  padding: 20px;
+}
+ </style>
+ 
+  <section id="projects" class="uk-section uk-section-small">
+    <div class="uk-container">
+<div class="row">
+      <h2 class="uk-text-center">Full Time Work</h2>
+       <p>During the past decade, or more, of my career, I had the privilege of working full-time for several companies, leading their digital marketing endeavors. My role primarily revolved around overseeing campaigns, coordinating schedules, and maintaining website updates. This encompassed a wide spectrum of my skill sets, spanning from UX design to proficient database management and efficient ad set supervision. Reflecting on this journey, I express profound gratitude to these employers who provided invaluable opportunities. I take pride in the accomplishments achieved during these collaborations and hold in high regard the skills honed through these transformative experiences.</p>
+        <div class="uk-flex uk-flex-center"><a href="/resume/" target="_blank" class="btn btn-primary" style="width:50%;">View Resume</a></div>
+</div>
+     
+<br><br>
+<div class="row">
+  <div class="col-sm-6">
+ <div class="card">
+ <img src="/logos/revobrands-logo-950x225.png" class="card-img-top" alt="Revo Brands">
+  <div class="card-body">
+    <h4 class="card-title">Revo Brands</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Web Design, UX Design</h6>
+        <p class="card-text">Revo Brands ignites growth in enthusiast and mission critical brands through superior innovation, design and marketing. We go to market through our brands Real Avid and Outdoor Edge.</p>
+        <a href="http://revobrands.com" target="_blank" class="btn btn-primary">Visit Revobrands.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/realavid-logo-950x225.png" class="card-img-top" alt="Real Avid">
+  <div class="card-body">
+    <h4 class="card-title">Real Avid</h4>
+    <h6 class="card-subtitle mb-2 text-muted">SEO, UX Design, Email Marketing</h6>
+        <p class="card-text">Real innovation and smart design have made Real Avid the go-to brand for Gun DIY® consumers. From Master Grade® tools to High Performance Gun CleaningTM and the world’s leading line of multi-tools for guns, Real Avid sets the high bar for real world performance.</p>
+        <a href="https://realavid.com" target="_blank" class="btn btn-primary">Visit RealAvid.com</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+  <div class="row">
+  <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/outdooredge-logo-950x225.png" class="card-img-top" alt="Outdoor Edge">
+  <div class="card-body">
+    <h4 class="card-title">Outdoor Edge</h4>
+    <h6 class="card-subtitle mb-2 text-muted">UX Design, SEO, Email Marketing</h6>
+        <p class="card-text">Outdoor Edge is a leading knife and tool brand that develops and uses innovative technology to create highly functional knives for hunters, fishers, outdoor enthusiasts, tradesmen and everyday carry. Their patented RazorSafeTM Blade System leads the industry in replaceable blade knives.</p>
+        <a href="https://outdooredge.com" target="_blank" class="btn btn-primary">Visit Outdooredge.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/dakotastones-logo-950x225.png" class="card-img-top" alt="Dakota Stones">
+  <div class="card-body">
+    <h4 class="card-title">Dakota Stones</h4>
+    <h6 class="card-subtitle mb-2 text-muted">ERP, Web Development, Email Marketing</h6>
+        <p class="card-text">Dakota Stones manufactures the only branded line of gemstone beads in the world.</p>
+        <a href="https://dakotastones.com" target="_blank" class="btn btn-primary">Visit Dakotastones.com</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row">
+ <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/goodybeads-logo-950x225.png" class="card-img-top" alt="Goody Beads">
+  <div class="card-body">
+    <h4 class="card-title">Goody Beads</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Database Management, Web Design, Email Marketing</h6>
+        <p class="card-text">At GoodyBeads, our mission is to bring you the latest jewelry-making trends. With over 20 years of experience, our staff has a great eye to keep you in the loop and make your projects shine.</p>
+        <a href="https://web.archive.org/web/20221002025812/https://goodybeads.com/collections/kits" target="_blank" class="btn btn-primary">Visit Goodybeads.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/ticasino-logo-950x225.png" class="card-img-top" alt="Treasure Island Casino">
+  <div class="card-body">
+    <h4 class="card-title">Treasure Island Casino</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Email Marketing, Video Graphics, SEO</h6>
+        <p class="card-text">Located along the Mississippi River, our breath-taking casino resort boasts fine and casual dining options, a full-service salon and spa, a family friendly water park, bowling alley, an event center and live entertainment venues including an outdoor amphitheater.</p>
+        <a href="https://ticasino.com" target="_blank" class="btn btn-primary">Visit TIcasino.com</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row">
+ <div class="col-sm-6">
+ <div class="card">
+  <img src="/logos/ainsleyshea-logo-950x225.png" class="card-img-top" alt="Ainsley Shea">
+  <div class="card-body">
+    <h4 class="card-title">Ainsley Shea</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Web Design, Brand Development</h6>
+        <p class="card-text">Our method is a bit different. We don't want to throw tactics at you. We sit down with a white board with your key people and get everyone to understand the story.</p>
+        <a href="https://web.archive.org/web/20120326013420/http://www.ainsleyshea.com/" target="_blank" class="btn btn-primary">Visit AinselyShea.com</a>
+      </div>
+    </div>
+  </div>      
+      </div>
+    </div>
+  </section>
+  
+  <!-- Client Projects Section -->
+  <section id="clients" class="uk-section uk-section-small">
+    <div class="uk-container"><br><br>
+      <h2 class="uk-text-center">Personal & Client Projects</h2>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Epic Ebike Adventures Website</h4>
+        <p class="card-text">A Shopify site for an ebike rental business in Las Vegas was successfully set up. The process involved choosing the right theme, customizing it to align with the brand, and adding all the necessary products, pages and features such as payment and shipping options, to ensure a seamless customer experience. The final result was a professional, user-friendly and functional online platform that allowed the business to showcase and rent their e-bikes to visitors and locals in Las Vegas.</p>
+        <a href="https://epicebikeadventures.com" target="_blank" class="btn btn-primary">Visit Epicebikeadventures.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Brisk Services Website</h4>
+        <p class="card-text">A successful WordPress website was crafted for a client by following a series of key steps. The foundation was laid by setting up a hosting account and installing WordPress, then selecting the perfect theme from a wide range of options. Customizing the theme with the theme editor or custom CSS brought the vision to life. Pages and posts were added, and functionality was boosted with plugins for features like contact forms, social media, and e-commerce. Navigation was made simple with a well-organized menu. Before launching, the website was thoroughly tested to ensure it was fully functional. The final product was a well-rounded and functional WordPress website for the client.</p>
+        <a href="http://briskservicesllc.com" target="_blank" class="btn btn-primary">Visit Briskservicesllc.com</a>
+      </div>
+    </div>
+  </div>
+  </div>
+  <br>
+  <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Driftless Camp Website</h4>
+        <p class="card-text">Crafting a successful WordPress website for a client involved several essential steps. The process started with setting up a hosting account and installing WordPress, followed by choosing the ideal theme from a vast collection of options. Customization, page and post creation, plugin integration, and menu organization resulted in a fully functional website ready for launch after thorough testing.</p>
+        <a href="https://driftlesscamp.com" target="_blank" class="btn btn-primary">Visit Driftlesscamp.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Bellflower Bodywork Website</h4>
+        <p class="card-text">The process of developing a massage therapy website for a client was a well-planned and executed project. Every step was taken to ensure the website was not only functional but also visually appealing to visitors. However, due to unforeseen circumstances, the website had to be taken down. Despite the setback, I remain committed to working with the client to find a solution to bring the site back online in the future.</p>
+        <a href="http://bellflowerbodywork.com" target="_blank" class="btn btn-primary">Visit Bellflowerbodywork.com</a>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Focused Fire Website</h4>
+        <p class="card-text">Firstly, a hosting account was set up and WordPress was installed, followed by choosing a fitting theme from the vast collection available. The chosen theme was then customized using the theme editor or custom CSS to bring the client's vision to life. Pages and posts were added, and functionality was enhanced through the use of plugins for features like contact forms, social media, and e-commerce. A navigation menu was created to make navigation easy for visitors, and the website was thoroughly tested before it was launched. The end result was a well-rounded and functional website that accurately reflected the client's laser engraving business.</p>
+        <a href="http://focusedfireengraving.com" target="_blank" class="btn btn-primary">Visit focusedfireengraving.com</a>
+      </div>
+    </div>
+  </div>
+</div>      
+      </div>
+    </div>
+  </section>
+  
+    <!-- Contact Section -->
+  <section id="contact" class="uk-section uk-section-small">
+    <div class="uk-container"><br><br>
+      <h2 class="uk-text-center">Contact</h2>
+
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me directly. I will get back to you within a matter of hours to help.</p>
+
+<form action="contact-form.php" method="post" class="container">
+  <div class="form-group">
+    <label for="email">Email:</label>
+    <input type="email" class="form-control" id="email" name="email" required>
+  </div>
+  <div class="form-group">
+    <label for="subject">Subject:</label>
+    <input type="text" class="form-control" id="subject" name="subject" required>
+  </div>
+  <div class="form-group">
+    <label for="message">Message:</label>
+    <textarea class="form-control" id="message" name="message" required></textarea>
+  </div>
+  <input type="submit" value="Send" class="btn btn-primary">
+</form>
+<br>
+<div class="uk-text-center">
+<strong>Email:</strong> <a href="mailto:andrew.michael.mcmorrow@gmail.com">andrew.michael.mcmorrow@gmail.com</a><br>
+<strong>Phone:</strong> <a href="tel:6123848959">612-384-8959</a><br>
+<strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/andrew-mcmorrow-734b3a19" target="_blank">Andrew McMorrow</a>
+</div>
+</div>
+  </section>
+  
+  
+
+  <!-- Footer -->
+  <footer class="uk-section uk-section-small uk-text-center">
+    <p>Copyright &copy; 2023 | Andrew McMorrow</p
